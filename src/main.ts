c3b8549774028a2a9b4ef14ec72d0af90970f9ed
unknown <arshsingh3755@gmail.com>
2022-12-24 22:54:34 +0530

    limit

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { json } from 'express';
 require('dotenv').config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(json({ limit: '50mb' }));
   await app.listen(parseInt(process.env.PORT) || 3000);
 }
 bootstrap();
